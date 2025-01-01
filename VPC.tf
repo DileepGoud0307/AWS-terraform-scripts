a19ccef23179180622542d795730e2affbe8651a
Dileep <dileepgoud0307@gmail.com>
2025-01-01 23:08:31 +0530

    updated VPC

--- VPC.tf
+++ VPC.tf
@@ -1,9 +1,9 @@
-# AWS Provider Configuration
+#AWS Provider Configuration
 provider "aws" {
   region = "ap-south-1" # Change this to your desired AWS region
 }
 
-# VPC Configuration
+#VPC Configuration
 resource "aws_vpc" "my_vpc" {
   cidr_block = "192.168.0.0/16"
   instance_tenancy = "default" 
